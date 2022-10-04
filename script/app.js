60facbcb0949972693d493519b24c9f12d5d22a0
HardikShahh563 <hardikts@gmail.com>
2022-10-04 12:24:04 +0530

    Modified web view

--- script/app.js
+++ script/app.js
@@ -1,5 +1,14 @@
 var navLines = document.querySelector(".nav-lines");
+var navGreyLine = document.querySelector(".nav-grey-line");
 navLines.addEventListener("click", () => {
     var navLinks = document.querySelector(".nav-links");
     navLinks.classList.toggle("active");
 })
+
+function screenWidthTest() {
+    if(screen.width < 1000) {
+        console.log("screen width less than 1000px");
+        navGreyLine.remove();
+    }
+}
+setInterval(screenWidthTest, 10);
